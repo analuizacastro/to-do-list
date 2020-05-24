2d12ada8152af8794a7216fee16275f2693a313a
Ana Luiza Castro <analuizabcastro@gmail.com>
2020-05-24 00:35:38 -0300

    chore: update design

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+   <img align="center" src=".github/todo-list.png" width="200"/>
+</p>
+<h2 align="center">
+  Todo List 🖍
+</h2>
+
+<p align="center">
+Organize your day and focus on what's important
+</p>
+<p align="center">
+   <a href="https://github.com/analuizacastro">
+      <img alt="Author" src="https://img.shields.io/badge/author-AnaLuizaCastro-blue">
+   </a>
+   <img alt="Languages" src="https://img.shields.io/github/languages/count/analuizacastro/to-do-list">
+</p>
+
+<br />
+<p align="center"><img src="/.github/project.png"/></p>
+
+---
+# :rocket: About the project
+
+Web application that allows you to organize your tasks, bringing more focus and productivity to your days. Hope you enjoy it.
+
+# :bulb: Installation
+
+**To clone the project via HTTPS, run this command:**
+
+```git clone https://github.com/analuizacastro/to-do-list.git```
+
+```git clone git@github.com:analuizacastro/to-do-list.git```
+
+Released in 2020.
+
+Made with love by [Ana Luiza Castro](https://github.com/AnaLuizaCastro) 💛
--- script.js
+++ script.js
@@ -1,90 +1,111 @@
 window.addEventListener("load", start);
 
-var globalNames = [];
+var globalNames = ['Skype with Mike', 'Walk my dog :)', 'Learn a new recipe'];
 var inputName = null;
+var isEditing = false;
 
-function start(){
-    inputName = document.querySelector('#inputName');
-    
-    preventFormSubmit();
-    activateInput();
+function start() {
+  inputName = document.querySelector("#inputName");
+  
+  preventFormSubmit();
+  activateInput();
+  render();
+}
+
+function preventFormSubmit() {
+  function handleFormSubtmit(event) {
+    event.preventDefault();
+  }
+  
+  var form = document.querySelector("form");
+  form.addEventListener("submit", handleFormSubtmit);
+}
+
+function activateInput() {
+  function insertName(newName) {
+    globalNames.push(newName);
     render();
+  }
+
+  function handleTyping() {
+    if (event.key === "Enter") {
+      if (isEditing) {
+      } else {
+        insertName(event.target.value);
+      }
+      isEditing = false;
+    }
+  }
+
+  inputName.addEventListener("keyup", handleTyping);
+  inputName.focus();
 }
 
-function preventFormSubmit(){
-    function handleFormSubtmit(event){
-        event.preventDefault();
+function render() {
+  function createDeleteButton(index) {
+    function deleteName(index) {
+      globalNames.splice(index, 1);
+      render();
     }
 
-    var form = document.querySelector('form');
-    form.addEventListener('submit', handleFormSubtmit);
+    // var button = document.createElement('button');
+    // button.textContent = '✓';
+    // button.classList.add('deleteButton');
+    // button.addEventListener('click', deleteName);
+
+    var icon = document.createElement("i");
+    icon.classList.add("material-icons");
+    icon.classList.add("md-48");
+    icon.classList.add("standardColor");
+    icon.classList.add("deleteButton");
+    icon.addEventListener("click", deleteName);
+    icon.textContent = "check_circle";
+    return icon;
+  }
+
+  function createSpan(name) {
+    function editItem() {
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+    }
+
+    var span = document.createElement("span");
+    span.classList.add('clickable')
+    span.textContent = name;
+    span.addEventListener("click", editItem);
+
+    return span;
+  }
+
+  var divNames = document.querySelector("#names");
+  divNames.innerHTML = "";
+
+  var ul = document.createElement("ul");
+
+  for (var i = 0; i < globalNames.length; i++) {
+    var currentName = globalNames[i];
+
+    var li = document.createElement("li");
+    var icon = createDeleteButton(i);
+    var span = createSpan(currentName);
+
+    var iconEdit = document.createElement("i");
+    iconEdit.classList.add("material-icons");
+    iconEdit.classList.add("dark-gray");
+    iconEdit.textContent = "post_add";
+
+    li.appendChild(icon);
+    // li.appendChild(button);
+    li.appendChild(span);
+    li.appendChild(iconEdit);
+    ul.appendChild(li);
+  }
+  divNames.appendChild(ul);
+  clearInput();
 }
 
-function activateInput(){
-    function insertName(newName){
-        globalNames.push(newName);
-        render();
-    }
-
-    function handleTyping(){
-        if(event.key === 'Enter'){
-            insertName(event.target.value);
-        }
-    }
-
-    inputName.addEventListener('keyup', handleTyping)
-    inputName.focus();
-}
-
-function render(){
-    function createDeleteButton(index){
-        function deleteName(index){
-            globalNames.splice(index, 1)
-            render();
-        }
-
-        // var button = document.createElement('button');
-        // button.textContent = '✓';
-        // button.classList.add('deleteButton');
-        // button.addEventListener('click', deleteName);
-
-        var icon = document.createElement('i');
-        icon.classList.add('material-icons');
-        icon.classList.add('md-48');
-        icon.classList.add('standardColor'); 
-        icon.classList.add('deleteButton');
-        icon.addEventListener('click', deleteName);
-        icon.textContent = 'check_circle';
-
-        return icon;
-    }
-    var divNames = document.querySelector('#names');
-    divNames.innerHTML = '';
-
-    var ul = document.createElement('ul');
-
-    for(var i = 0; i < globalNames.length; i++){
-        var currentName = globalNames[i];
-
-        var li = document.createElement('li');
-        var icon = createDeleteButton(i);
-
-        var span = document.createElement('span');
-        span.textContent = currentName;
-
-
-
-        li.appendChild(icon);
-        // li.appendChild(button);
-        li.appendChild(span);
-
-        ul.appendChild(li);
-    }
-    divNames.appendChild(ul);
-    clearInput();
-}
-
-function clearInput(){
-    inputName.value = '';
-    inputName.focus();
+function clearInput() {
+  inputName.value = "";
+  inputName.focus();
 }
